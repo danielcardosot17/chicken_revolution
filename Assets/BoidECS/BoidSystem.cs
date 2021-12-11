964d69f6941a6bbf87f8c8a156863f4f79b53447
Daniel Cardoso <danielcardosot17@gmail.com>
2021-12-11 18:43:43 -0300

    boids with jobs deprecated method

--- Assets/BoidECS/BoidSystem.cs
+++ Assets/BoidECS/BoidSystem.cs
@@ -1,96 +1,141 @@
-using Unity.Burst;
-using Unity.Collections;
-using Unity.Entities;
-using Unity.Jobs;
-using Unity.Mathematics;
-using Unity.Transforms;
+// using Unity.Burst;
+// using Unity.Collections;
+// using Unity.Entities;
+// using Unity.Jobs;
+// using Unity.Mathematics;
+// using Unity.Transforms;
+// using UnityEngine;
 
-public class BoidSystem : SystemBase
-{
-    private BoidController controller;
+// public class BoidSystem : SystemBase
+// {
+//     private BoidController controller;
 
-    protected override void OnUpdate()
-    {
-        if(!controller)
-        {
-            controller = BoidController.Instance;
-        }
-        if(controller)
-        {
-            float boidPerceptionRadius = controller.boidPerceptionRadius;
-            float separationWeight = controller.separationWeight;
-            float cohesionWeight = controller.cohesionWeight;
-            float alignmentWeight = controller.alignmentWeight;
-            float cageSize = controller.cageSize;
-            float avoidWallsTurnDist = controller.avoidWallsTurnDist;
-            float avoidWallsWeight = controller.avoidWallsWeight;
-            float boidSpeed = controller.boidSpeed;
-            float deltaTime = Time.DeltaTime;
+//     // protected override void OnDestroy() {
+//     //     if(boidIdDataArray.IsCreated)
+//     //     {
+//     //         boidIdDataArray.Dispose();
+//     //     }
+
+//     //     if(boidLocalToWorldArray.IsCreated)
+//     //     {
+//     //         boidLocalToWorldArray.Dispose();
+//     //     }
+
+//     //     if(newBoidPositions.IsCreated)
+//     //     {
+//     //         newBoidPositions.Dispose();
+//     //     }
+//     //     base.OnDestroy();
+//     // }
+
+//     protected override void OnUpdate()
+//     {
+
+//         // if(boidIdDataArray.IsCreated)
+//         // {
+//         //     boidIdDataArray.Dispose();
+//         // }
+//         // boidIdDataArray = new NativeArray<BoidIdData>(boidQuery.CalculateEntityCount(),Allocator.TempJob);
+
+//         // if(boidLocalToWorldArray.IsCreated)
+//         // {
+//         //     boidLocalToWorldArray.Dispose();
+//         // }
+//         // boidLocalToWorldArray = new NativeArray<LocalToWorld>(boidQuery.CalculateEntityCount(),Allocator.TempJob);
+
+//         // if(newBoidPositions.IsCreated)
+//         // {
+//         //     newBoidPositions.Dispose();
+//         // }
+//         // newBoidPositions = new NativeArray<float4x4>(boidQuery.CalculateEntityCount(), Allocator.TempJob);
+
+//         if(!controller)
+//         {
+//             controller = BoidController.Instance;
+//         }
+//         if(controller)
+//         {
+//             float boidPerceptionRadius = controller.boidPerceptionRadius;
+//             float separationWeight = controller.separationWeight;
+//             float cohesionWeight = controller.cohesionWeight;
+//             float alignmentWeight = controller.alignmentWeight;
+//             float cageSize = controller.cageSize;
+//             float avoidWallsTurnDist = controller.avoidWallsTurnDist;
+//             float avoidWallsWeight = controller.avoidWallsWeight;
+//             float boidSpeed = controller.boidSpeed;
+//             float deltaTime = Time.DeltaTime;
             
-
-            EntityQuery boidQuery = GetEntityQuery(ComponentType.ReadOnly<BoidIdData>(),ComponentType.ReadOnly<BoidSharedData>(),ComponentType.ReadOnly<LocalToWorld>());
-            
-            NativeArray<BoidIdData> boidIdDataArray = boidQuery.ToComponentDataArray<BoidIdData>(Allocator.Temp);
-
-            NativeArray<LocalToWorld> boidLocalToWorldArray = boidQuery.ToComponentDataArray<LocalToWorld>(Allocator.Temp);
-
-            NativeArray<float4x4> newBoidPositions = new NativeArray<float4x4>(boidQuery.CalculateEntityCount(), Allocator.Temp);
-
-            Entities.WithAll<BoidSharedData>().ForEach((ref LocalToWorld localToWorld, ref BoidIdData boidData) => {
-                int boidIndex = boidData.boidIndex;
-                float3 boidPosition = localToWorld.Position;
+//             EntityQuery boidQuery = GetEntityQuery(ComponentType.ReadOnly<BoidIdData>(),ComponentType.ReadOnly<BoidSharedData>(),ComponentType.ReadOnly<LocalToWorld>());
                 
-                float3 seperationSum = float3.zero;
-                float3 positionSum = float3.zero;
-                float3 headingSum = float3.zero;
+//             NativeArray<BoidIdData>  boidIdDataArray = boidQuery.ToComponentDataArray<BoidIdData>(Allocator.Temp);
 
-                for(int i = 0; i < boidIdDataArray.Length; i++)
-                {
-                    if (boidIndex != boidIdDataArray[i].boidIndex) {
+//             NativeArray<LocalToWorld> boidLocalToWorldArray = boidQuery.ToComponentDataArray<LocalToWorld>(Allocator.Temp);
+
+//             NativeArray<float4x4> newBoidPositions = new NativeArray<float4x4>(boidQuery.CalculateEntityCount(), Allocator.Temp);
+
+//             Entities.WithAll<BoidSharedData>().ForEach((ref LocalToWorld localToWorld, ref BoidIdData boidData) => {
+//                 int boidIndex = boidData.boidIndex;
+//                 float3 boidPosition = localToWorld.Position;
+                
+//                 float3 seperationSum = float3.zero;
+//                 float3 positionSum = float3.zero;
+//                 float3 headingSum = float3.zero;
+
+//                 for(int i = 0; i < boidIdDataArray.Length; i++)
+//                 {
+//                     if (boidIndex != boidIdDataArray[i].boidIndex) {
                         
-                        float distToOtherBoid = math.length(boidPosition - boidLocalToWorldArray[i].Position);
-                        if (distToOtherBoid < boidPerceptionRadius) {
+//                         float distToOtherBoid = math.length(boidPosition - boidLocalToWorldArray[i].Position);
+//                         if (distToOtherBoid < boidPerceptionRadius) {
 
-                            seperationSum += -(boidLocalToWorldArray[i].Position - boidPosition) * (1f / math.max(distToOtherBoid, .0001f));
-                            positionSum += boidLocalToWorldArray[i].Position;
-                            headingSum += boidLocalToWorldArray[i].Forward;
-                            boidData.boidsNearby++;
-                        }
-                    }
-                }
+//                             seperationSum += -(boidLocalToWorldArray[i].Position - boidPosition) * (1f / math.max(distToOtherBoid, .0001f));
+//                             positionSum += boidLocalToWorldArray[i].Position;
+//                             headingSum += boidLocalToWorldArray[i].Forward;
+//                             boidData.boidsNearby++;
+//                         }
+//                     }
+//                 }
 
-                float3 force = float3.zero;
+//                 float3 force = float3.zero;
 
-                if (boidData.boidsNearby > 0) {
-                    force += (seperationSum / boidData.boidsNearby)                * separationWeight;
-                    force += ((positionSum / boidData.boidsNearby) - boidPosition) * cohesionWeight;
-                    force += (headingSum / boidData.boidsNearby)                   * alignmentWeight;
-                }
-                if (math.min(math.min(
-                    (cageSize / 2f) - math.abs(boidPosition.x),
-                    (cageSize / 2f) - math.abs(boidPosition.y)),
-                    (cageSize / 2f) - math.abs(boidPosition.z))
-                        < avoidWallsTurnDist) {
-                    force += -math.normalize(boidPosition) * avoidWallsWeight;
-                }
+//                 if (boidData.boidsNearby > 0) {
+//                     force += (seperationSum / boidData.boidsNearby)                * separationWeight;
+//                     force += ((positionSum / boidData.boidsNearby) - boidPosition) * cohesionWeight;
+//                     force += (headingSum / boidData.boidsNearby)                   * alignmentWeight;
+//                 }
+//                 if (math.min(math.min(
+//                     (cageSize / 2f) - math.abs(boidPosition.x),
+//                     (cageSize / 2f) - math.abs(boidPosition.y)),
+//                     (cageSize / 2f) - math.abs(boidPosition.z))
+//                         < avoidWallsTurnDist) {
+//                     force += -math.normalize(boidPosition) * avoidWallsWeight;
+//                 }
 
-                float3 velocity = localToWorld.Forward * boidSpeed;
-                velocity += force * deltaTime;
-                velocity = math.normalize(velocity) * boidSpeed;
+//                 float3 velocity = localToWorld.Forward * boidSpeed;
+//                 velocity += force * deltaTime;
+//                 velocity = math.normalize(velocity) * boidSpeed;
 
-                newBoidPositions[boidIndex] = float4x4.TRS(
-                    localToWorld.Position + velocity * deltaTime,
-                    quaternion.LookRotationSafe(velocity, localToWorld.Up),
-                    new float3(1f)
-                );
-            }).Run();
+//                 newBoidPositions[boidIndex] = float4x4.TRS(
+//                     localToWorld.Position + velocity * deltaTime,
+//                     quaternion.LookRotationSafe(velocity, localToWorld.Up),
+//                     new float3(1f)
+//                 );
+                
+//                 // localToWorld.Value = newBoidPositions[boidIndex];
+//             })
+//             // .WithDisposeOnCompletion(idDataArray)
+//             // .WithDisposeOnCompletion(localToWorldArray)
+//             // .WithDisposeOnCompletion(newPositions)
+//             .Run();
             
-            Entities.WithAll<BoidSharedData>().ForEach((ref LocalToWorld localToWorld, ref BoidIdData boidData) => {
-                localToWorld.Value = newBoidPositions[boidData.boidIndex];
-            }).Run();
+//             Entities.WithAll<BoidSharedData>().ForEach((ref LocalToWorld localToWorld, ref BoidIdData boidData) => {
+//                 localToWorld.Value = newBoidPositions[boidData.boidIndex];
+//             })
+//             // .WithDisposeOnCompletion(idDataArray)
+//             // .WithDisposeOnCompletion(localToWorldArray)
+//             // .WithDisposeOnCompletion(newPositions)
+//             .Run();
 
-        }
-    }
-
-    
-}
+//         }
+//     }
+// }
