d71a9a86613ea6b2c0b60dfca3dc3b44fbe90c11
Daniel Cardoso <danielcardosot17@gmail.com>
2021-12-13 22:46:01 -0300

    Audio events working. Next: adjust Behaviour weigts with player movement and adjust shortCry audios

--- Assets/Scripts/Monobehaviours/PlayerFollower.cs
+++ Assets/Scripts/Monobehaviours/PlayerFollower.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,22 @@ public class PlayerFollower : MonoBehaviour
     public Transform target;
     public float radius;
     public float followSpeed;
+    
+    [Range(0f,5f)]
+    public float followDelay = 2;
+
+    public AudioEventSO warcryLong;
+    public AudioEventSO warcryShort;
+    public AudioEventSO flockStandby;
+    public GameEventSO stopCurrentAudio;
+
+    private float timer = 0;
+    private bool isScreaming = false;
+
+    private void Start() {
+        flockStandby.Raise();
+        timer = followDelay;
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,12 +31,51 @@ public class PlayerFollower : MonoBehaviour
         Vector3 direction = target.position - transform.position;
         if(direction.magnitude > radius)
         {
-            transform.position += direction.normalized * followSpeed * Time.deltaTime;
+            if(timer <= 0)
+            {
+                if(!isScreaming)
+                {
+                    if(((direction.magnitude - radius) / followSpeed) < followDelay)
+                    {
+                        warcryShort.Raise();
+                    }
+                    else
+                    {
+                        warcryLong.Raise();
+                    }
+                    isScreaming = true;
+                }
+                transform.position += direction.normalized * followSpeed * Time.deltaTime;
+            }
+            else
+            {
+                timer -= Time.deltaTime;
+            }
         }        
+        else
+        {
+            if(isScreaming)
+            {
+                StartCoroutine(DoAfterTimeCoroutine(followDelay, () => 
+                {
+                    isScreaming = false;
+                    stopCurrentAudio.Raise();
+                    flockStandby.Raise();
+                }));
+            }
+            timer = followDelay;
+        }
     }
     private void OnDrawGizmos() {
 
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position,radius);
+    }
+
+    
+    public static IEnumerator DoAfterTimeCoroutine(float time, Action action)
+    {
+        yield return new WaitForSeconds(time);
+        action();
     }
 }
